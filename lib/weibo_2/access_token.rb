f60c4557482f203284839bfe41f901d380566418
simsicon <simsicon@gmail.com>
2012-07-19 18:32:26 +0800

    Add Time convertion, ensure expired validation in access_token would be correct in whatever server on this xxxxxxx planet.

--- lib/weibo_2/access_token.rb
+++ lib/weibo_2/access_token.rb
@@ -4,5 +4,20 @@ module WeiboOAuth2
     def validated?
       !!@expires_at && !expired?
     end
+
+    # Whether or not the token is expired
+    #
+    # @return [Boolean]
+    def expired?
+      expires? && (expires_at < time_convertion(Time.now, '+08:00').to_i)
+    end
+
+    #Convert Time from a time zone to another time zone
+    #'+08:00' or '-08:00'
+    #return Time
+    def self.time_convertion(time, time_zone)
+        t = time.utc
+        Time.new(t.year, t.month, t.day, t.hour, t.min, t.sec, time_zone)
+    end  
   end
 end
