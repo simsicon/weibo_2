07b9ab38d876c834dfe5b47a5ea8516700c2ec75
zhl <zhouhualei@gmail.com>
2013-04-14 16:39:22 +0800

    add access_token in client

--- lib/weibo_2/client.rb
+++ lib/weibo_2/client.rb
@@ -2,7 +2,9 @@ require 'oauth2'
 
 module WeiboOAuth2
   class Client < OAuth2::Client
- 
+
+    attr_accessor :access_token
+    
     def initialize(client_id='', client_secret='', opts={}, &block)
       client_id = WeiboOAuth2::Config.api_key if client_id.empty?
       client_secret = WeiboOAuth2::Config.api_secret if client_secret.empty?
@@ -11,13 +13,20 @@ module WeiboOAuth2
       @options[:authorize_url] = '/oauth2/authorize'
       @options[:token_url] = '/oauth2/access_token'
     end
-    
+
+    def self.from_code(code, opts={}, &block)
+      client = self.new(opts, &block)
+      client.auth_code.get_token(code)
+
+      client
+    end
+
     def self.from_hash(hash, opts={}, &block)
       client = self.new(opts, &block)
       client.get_token_from_hash(hash)
 
       client
-    end
+    end    
 
     def authorize_url(params={})
       params[:client_id] = @id unless params[:client_id]
