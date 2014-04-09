8e033c1d9c3d522704c0d7354ceee01d02c77b99
simsicon <simsicon@gmail.com>
2014-04-09 14:45:42 +0800

    Bump version to 0.1.6

--- lib/weibo_2/version.rb
+++ lib/weibo_2/version.rb
@@ -1,9 +1,9 @@
 module WeiboOAuth2
-  
+
   class Version
     MAJOR = 0 unless defined? MAJOR
     MINOR = 1 unless defined? MINOR
-    PATCH = 5 unless defined? PATCH
+    PATCH = 6 unless defined? PATCH
     PRE = nil unless defined? PRE
 
     class << self
