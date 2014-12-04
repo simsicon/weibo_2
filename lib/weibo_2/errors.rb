cf99d1232facd5fa5a786ccefa2f7361de3e4747
ianchoi <ian@rebaselabs.com>
2014-12-04 15:15:03 -0500

    Improvement to error handling by identifying OAuth2 errors as specific errors.
    Currently all errors are raised as Oauth2 errors.

---
+++ lib/weibo_2/errors.rb
@@ -0,0 +1,20 @@
+module WeiboOAuth2
+  module Errors
+    class WeiboError < StandardError
+      attr_reader :data
+      def initialize(data)
+        @data = data
+        super
+      end
+    end
+
+    class GeneralError < WeiboError
+    end
+
+    class RateLimitedError < WeiboError
+    end
+
+    class UnauthorizedError < WeiboError
+    end
+  end
+end
